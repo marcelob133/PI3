5ddb8ab95ca9522a533ee725660add63902da6cf
Marcelo B H <marceloborellihegedus@gmail.com>
2018-06-07 17:38:40 -0300

    general modifications
    modifications

---
+++ EC_Solution/WebAppEC/Models/EsqueciSenhaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppEC.Models
+{
+    [NotMapped]
+    public class EsqueciSenhaViewModel
+    {
+        [Required(ErrorMessage = "E-mail é obrigatório")]
+        [StringLength(100, MinimumLength = 5)]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        [DisplayName("E-mail")]
+        public virtual string emailCliente { get; set; }
+    }
+}
